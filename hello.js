125c3dfecab44e91c1917c7eabc1222159945a48
Grant <grant@grants-mbp.main.local.una.edu>
2023-08-18 11:08:05 -0500

    changed message
    Hello from node

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const Message = "Hello World";
+const Message = "Hello from node";
 app.get("/", (_req, res) => {
     res.send(Message);
 });
