2c1ab75239236c829415fdfd2896abf25c101f74
Grant <grant@grants-mbp.main.local.una.edu>
2023-09-19 13:29:34 -0500

    inited npm project

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const route = express()
 const bodyParser = require("body-parser")
-const port = 3000
+const port = process.env.PORT || 3000
 const portArray = [80, 443, undefined, NaN, null]
 
 route.set("view engine", "ejs")
@@ -37,3 +37,32 @@ if (!portArray.includes(port)) {
 } else {
     console.log("Invalid Port")
 }
+
+const { MongoClient, ServerApiVersion } = require("mongodb")
+const uri =
+    "mongodb+srv://granthartley:2VSvwD3Aqhpa3AwD@cluster0.sh7wwbq.mongodb.net/?retryWrites=true&w=majority"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect()
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 })
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close()
+    }
+}
+run().catch(console.dir)
